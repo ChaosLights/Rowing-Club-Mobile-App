ca58094fa640c51a55b8bd1423b5ab58153266fe
efysm8 <efysm8@nottingham.ac.uk>
2024-02-15 02:16:47 +0000

    Settings Page - Updated password change functionality to check against Firestore

--- rowing_club_app/src/screens/settings/settings.js
+++ rowing_club_app/src/screens/settings/settings.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, Alert } from 'react-native';
 import Theme from '../../style';
+import { db } from '../../config/firebase';
+import { doc, updateDoc, getDoc } from 'firebase/firestore';
 
 export default function SettingsScreen({ navigation }) {
     const [currentPassword, setCurrentPassword] = useState('');
     const [newPassword, setNewPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
 
-    const handleChangePassword = () => {
+    const handleChangePassword = async () => {
 
         if (currentPassword === '' || newPassword === '' || confirmPassword === '') {
             Alert.alert('Please fill in all fields');
@@ -17,6 +19,36 @@ export default function SettingsScreen({ navigation }) {
         if (newPassword !== confirmPassword) {
             Alert.alert('New password and confirm password do not match');
             return;
+        }
+
+        const passwordDocumentId = 'ZcF4gkJykIoiwl593D6U';
+
+        try {
+            const passwordDocRef = doc(db, 'Passwords', passwordDocumentId);
+            const passwordDocSnap = await getDoc(passwordDocRef);
+
+            if (!passwordDocSnap.exists()) {
+                Alert.alert('No password document found')
+                return;
+            }
+        
+
+            const storedPassword = passwordDocSnap.data().currentUserPassword;
+
+            if (storedPassword !== currentPassword) {
+                Alert.alert('Current passwrd is incorrect');
+                return;
+            }
+
+            await updateDoc(passwordDocRef, {
+                currentUserPassword: newPassword,
+            });
+
+            Alert.alert('Password updated successfully');
+
+        } catch (error) {
+            console.error("Error updating password: ", error);
+            Alert.alert('There was a problem updating the password.');
         }
 
         console.log('New password:', newPassword);
