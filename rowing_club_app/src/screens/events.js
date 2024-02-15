8627ed552ff40bbd103e9cf58f494337f0ebd697
Prakriti Rai <efypr1@exmail.nottingham.ac.uk>
2024-02-15 13:47:00 +0000

    Editing events page and editing style sheet

--- rowing_club_app/src/screens/events.js
+++ rowing_club_app/src/screens/events.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { db } from '../config/firebase';
-import {collection, onSnapshot} from "firebase/firestore";
+import { collection, onSnapshot, query, where, orderBy} from "firebase/firestore";
 import Theme from '../style';
 
-export default function EventsScreen({navigation}) {
+export default function EventsScreen({ navigation }) {
     // const
     const [events, addEvents] = useState([]);
-    
-    // get events
-    useEffect(() => {
-        onSnapshot(collection(db, "Event"), (snapshot) => {
-            let eventList = []
-            snapshot.docs.map((doc) => eventList.push({ ...doc.data(), id: doc.id }))
-            addEvents(eventList)
-        })
-    }, [])
+
+    // Fetch events for U13
+    const fetchU13Events = async () => {
+        const q = query(collection(db, "Event"),
+            where("category", "==", "Younger"),
+            orderBy("date", "asc") // Order by timestamp in ascending order
+        );
+        const querySnapshot = await onSnapshot(q, (snapshot) => {
+            let eventList = [];
+            snapshot.docs.map((doc) => eventList.push({ ...doc.data(), id: doc.id }));
+            addEvents(eventList);
+        });
+    };
+
+    // Fetch events for U18
+    const fetchU18Events = async () => {
+        const q = query(collection(db, "Event"),
+            where("category", "==", "Older"),
+            orderBy("date", "asc") // Order by timestamp in ascending order
+        );
+        const querySnapshot = await onSnapshot(q, (snapshot) => {
+            let eventList = [];
+            snapshot.docs.map((doc) => eventList.push({ ...doc.data(), id: doc.id }));
+            addEvents(eventList);
+        });
+    };
 
     // for each event item
-    const renderItem = ({item}) => (
-        <View style={Theme.view}>
+    const renderItem = ({ item }) => (
+        <View style={Theme.eventContainer}>
             <Text style={Theme.h2}>
                 {item.title}
             </Text>
@@ -32,17 +49,26 @@ export default function EventsScreen({navigation}) {
                 {item.description}
             </Text>
         </View>
-    )
+    );
+
+
     // get date in format
     function dateFormat(date) {
-        return new Date(date.seconds * 1000).toLocaleString()
-      }
+        return new Date(date.seconds * 1000).toLocaleString();
+    }
 
     // main
     return (
-        <View style={Theme.view}>
-            <Text style={Theme.title}>Events Page Implementation...{"\n"}</Text>
+        <View style={Theme.container}>
+            <View style={Theme.optionBar}>
+                <TouchableOpacity style={Theme.optionBarButton} onPress={fetchU13Events}>
+                    <Text style={Theme.optionText}>U13</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={Theme.optionBarButton} onPress={fetchU18Events}>
+                    <Text style={Theme.optionText}>U18</Text>
+                </TouchableOpacity>
+            </View>
             <FlatList data={events} renderItem={renderItem} keyExtractor={item => item.id} />
         </View>
-    )
+    );
 }
