1c9024fe41f4865005fd11f6539102da4790eab8
Yasmin Gordon <psyyg4@nottingham.ac.uk>
2024-02-28 22:23:33 +0000

    Home Page - adding coach view

---
+++ rowing_club_app/src/screens/home/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import HomeRower from './homeRower';
+import HomeCoach from './homeCoach';
+
+export default function EventsScreen({ navigation }) {
+    userType = "Coach"
+    //userType = "14-15"
+
+    if(userType == "Coach") {
+        return (
+            <HomeCoach/>
+        );
+    } else {
+        return (
+            <HomeRower/>
+        );
+    }
+}
---
+++ rowing_club_app/src/screens/home/homeCoach.js
@@ -0,0 +1,147 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, ScrollView, FlatList } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { db } from '../../config/firebase';
+import { collection, onSnapshot } from "firebase/firestore";
+import Theme from '../../style';
+
+export default function HomeScreen({ navigation }) {
+    //CONSTS
+    const [attendance, addAttendance] = useState([]);
+    const [notification, addNotification] = useState([]);
+    const [sessionAttendance, setSessionAttendance] = useState({}); // State to track attendance for each session
+    const [selectedWeek, setSelectedWeek] = useState('currentWeek'); // State to track selected week
+
+    const typeID = "AmU8s77q7TcDytflxrC8"; // id for over 18
+    //const typeID = "Onulbd9Ck9DoxPDN1bZ1"; //id for 14-15
+
+    //GET GROUP ATTENDANCE SCHEDULE
+    //from RecuringSchedule db
+    useEffect(() => {
+        console.log("Fetching attendance schedule...");
+        onSnapshot(collection(db, "RecuringSchedule"), (snapshot) => {
+            let attendanceList = []
+            snapshot.docs.forEach((doc) => {
+                const attendanceData = { ...doc.data(), id: doc.id };
+                if (attendanceData.TypeID === typeID) {
+                    attendanceList.push(attendanceData);
+                    addAttendance(attendanceList);
+                    return;
+                }
+            });
+        });
+    }, []);
+
+    //GET NOTIFIACTIONS
+    //from Notification db
+    useEffect(() => {
+        console.log("Fetching notifications...");
+        onSnapshot(collection(db, "Notification"), (snapshot) => {
+            let notificationList = []
+            snapshot.docs.map((doc) => notificationList.push({ ...doc.data(), id: doc.id }))
+            addNotification(notificationList)
+        })
+    }, [])
+
+    const renderNotification = ({ item }) => (
+        <View style={Theme.eventContainer}>
+            <Text style={Theme.h2}>
+                {item.Overview}
+            </Text>
+            <Text style={Theme.body}>
+            {item.Description}
+            </Text>
+        </View>
+    );
+
+    // Function to handle attendance selection
+    const handleAttendanceSelection = (sessionIndex, value) => {
+        console.log(`Session ${sessionIndex} attendance set to: ${value}`);
+        setSessionAttendance(prevState => ({
+            ...prevState,
+            [sessionIndex]: value // Update attendance for the selected session
+        }));
+    }
+
+    // displays week titles
+    // and calls renderWeek function
+    // (called in main return function)
+    const renderAttendance = ({ item }) => (
+        <View style={Theme.view}>
+            <Text style={Theme.h2}>{"\n"}Sessions:
+            </Text>
+            <View style={{ flexDirection: 'row' }}>
+                <View style={{ flex: 1 }}>
+                    <Text style={Theme.h3}>Current Week</Text> 
+                    {renderWeek(item.Sessions, weekdays, 0)} 
+                </View>
+                <View style={{ flex: 1 }}>
+                    <Text style={Theme.h3}>Next Week</Text> 
+                    {renderWeek(item.Sessions, weekdays, 7)}
+                </View>
+            </View>
+        </View>
+    );
+
+
+    // DISPLAY TRAINING SESSIONS
+    // set current date, and date of the start of the week
+    const currentDate = new Date();
+    const startOfWeek = new Date(currentDate);
+    startOfWeek.setDate(currentDate.getDate() - currentDate.getDay() + (currentDate.getDay() === 0 ? -6 : 1));
+
+    // makes weekdays list, storing all dates within the current week
+    const weekdays = [];
+    for (let i = 0; i < 7; i++) {
+        const day = new Date(startOfWeek);
+        day.setDate(startOfWeek.getDate() + i);
+        const weekday = day.toLocaleDateString(undefined, { weekday: 'long' });
+        weekdays.push({ fullDate: day, weekday: weekday });
+    }
+
+    // displays all training info
+    // (called in renderAttendance function)
+    const renderWeek = (sessions, weekdays, offset) => {
+        return weekdays.map((dayObj, index) => {
+            const targetDate = new Date(dayObj.fullDate);
+            targetDate.setDate(targetDate.getDate() + offset);
+
+            const sessionsForDay = sessions.filter((s) => {
+                const [day] = s.split(', ');
+                return day === targetDate.toLocaleDateString(undefined, { weekday: 'long' });
+            });
+
+            return (
+                // prints weekday and training times
+                <View key={index} style={Theme.eventContainer}>
+                    <Text style={Theme.h3}>{targetDate.toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</Text>
+                    {sessionsForDay.length > 0 ? (
+                        sessionsForDay.map((session, sessionIndex) => (
+                            <View key={sessionIndex} style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                                <Text>{session.split(', ')[1]}</Text>
+                                
+                            </View>
+                        ))
+                    ) : (
+                        <Text>No session{"\n"}</Text>
+                    )}
+
+                </View>
+            );
+        });
+    };
+
+    // MAIN 
+    // prints headings and calls methods renderNotification and renderAttendance to display info
+    return (
+        <ScrollView>
+            <View style={Theme.view}>
+                <Text style={Theme.title}>Home Page Implementation! {"\n"} </Text>
+                <Text style={Theme.h1}>Notifications</Text>
+                <FlatList data={notification} renderItem={renderNotification} keyExtractor={item => item.id} />
+                <Text style={Theme.h1}>{"\n"}Attendance</Text>
+                <FlatList data={attendance} renderItem={renderAttendance} keyExtractor={item => item.id} />
+            </View>
+        </ScrollView>
+    );
+}
