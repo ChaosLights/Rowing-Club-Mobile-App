838a30efd835338f49d3d1bc8e0d006e8ec8150b
Prakriti Rai <efypr1@exmail.nottingham.ac.uk>
2024-03-07 13:24:02 +0000

    Adding floating buttons feature for adding and removing events, editing stylesheet to adjust the dropdown list sizing

--- rowing_club_app/App.js
+++ rowing_club_app/App.js
@@ -3,7 +3,7 @@ import ScreensContainer from './src/screensContainer';
 
 export default function App() {
   global.user = "FWBWX7EOw75rwE20cQD2" //Coach
-  // global.user = "0Iz45PGQ70hFnUpo6loC" //14-15
+  //global.user = "0Iz45PGQ70hFnUpo6loC" //14-15
   // global.user = "njxPjxAazoa9feL0eeKF" //18 and Older
 
   return (
