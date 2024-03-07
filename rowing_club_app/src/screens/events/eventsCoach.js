0d3667916c335f3f042f6350ea77c44d72e4965f
Prakriti Rai <efypr1@exmail.nottingham.ac.uk>
2024-03-07 13:24:02 +0000

    Adding floating buttons feature for adding and removing events, editing stylesheet to adjust the dropdown list sizing

--- rowing_club_app/src/screens/events/eventsCoach.js
+++ rowing_club_app/src/screens/events/eventsCoach.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { View, Text, FlatList, TouchableOpacity, TextInput } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, TextInput, StyleSheet, Animated} from 'react-native';
 import { MultipleSelectList } from 'react-native-dropdown-select-list'
 import { db } from '../../config/firebase';
 import { collection, onSnapshot, query, where, orderBy } from "firebase/firestore";
 import Theme from '../../style';
 import * as eventRender from './eventRender';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function EventsCoach({ navigation }) {
     // const
@@ -13,6 +14,9 @@ export default function EventsCoach({ navigation }) {
     const [selected, setSelected] = useState([]);
     const [userTypeList, setUserTypeList] = useState([]);
     const [editEvent, setEditEvent] = useState();
+    const [icon1] = useState(new Animated.Value(-160));
+    const [icon2] = useState(new Animated.Value(80));
+    const [pop, setPop] = useState(false);
 
     // get dropdown options
     useEffect(() => {
@@ -76,24 +80,97 @@ export default function EventsCoach({ navigation }) {
         return new Date(date.seconds * 1000).toLocaleString();
     }
 
-    // main
-    return (
-        <View style={Theme.container}>
-            <Text style={Theme.body}>{"\n"}</Text>
-            <MultipleSelectList // dropdown for different rower types
-                setSelected={(val) => setSelected(val)}
-                search = {false}
-                data = {userTypeList}
-                boxStyles={{width: '90%'}} // MOVE TO STYLE SHEET??
-                save="key"
-            />
+  // handle button animation
+  const popIn = () => {
+    setPop(true);
+    Animated.timing(icon1, {
+      toValue: -160,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(icon2, {
+      toValue: 80,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  }
 
-            <View style={Theme.optionBar}>
-                <TouchableOpacity style={Theme.optionBarButton} onPress={fetchEvents}>
-                    <Text style={Theme.optionText}>Search</Text>
-                </TouchableOpacity>
-            </View>
-            <FlatList data={coachEvents} renderItem={renderItem} keyExtractor={item => item.id} />
+  const popOut = () => {
+    setPop(false);
+    Animated.timing(icon1, {
+      toValue: -90,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(icon2, {
+      toValue: -60,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  }
+
+    // main
+  return (
+    <View style={styles.container}>
+      <View style={styles.contentContainer}>
+        <MultipleSelectList // dropdown for different rower types
+          setSelected={(val) => setSelected(val)}
+          search={false}
+          data={userTypeList}
+          boxStyles={Theme.dropdownContainer}
+          save="key"
+        />
+        <View style={Theme.optionBar}>
+          <TouchableOpacity style={Theme.optionBarButton} onPress={fetchEvents}>
+            <Text style={Theme.optionText}>Search</Text>
+          </TouchableOpacity>
         </View>
-    );
+        <FlatList data={coachEvents} renderItem={renderItem} keyExtractor={(item) => item.id} />
+      </View>
+      <View style={styles.floatingButtonContainer}>
+        <Animated.View style={[styles.circle, { bottom: icon1 }]}>
+          <TouchableOpacity>
+            <Icon name="plus" size={25} color="#FFFF" />
+          </TouchableOpacity>
+        </Animated.View>
+        <Animated.View style={[styles.circle, { top: icon2 }]}>
+          <TouchableOpacity>
+            <Icon name="trash" size={25} color="#FFFF" />
+          </TouchableOpacity>
+        </Animated.View>
+        <TouchableOpacity
+          style={styles.circle}
+          onPress={() => {
+            pop === false ? popIn() : popOut();
+          }}
+        >
+          <Icon name="pencil" size={25} color="#FFFF" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative',
+  },
+  contentContainer: {
+    flex: 1,
+  },
+  floatingButtonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+  },
+  circle: {
+    backgroundColor: 'maroon',
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+});
--- rowing_club_app/src/style.js
+++ rowing_club_app/src/style.js
@@ -27,32 +27,48 @@ const Theme = StyleSheet.create({
         borderBottomWidth: 1,
         borderBottomColor: 'black',
     },
-    optionBar: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center', 
-        marginTop: 10,
-        backgroundColor: 'maroon',
-        borderRadius: 40,
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        width: '80%',
-        alignSelf: 'center',
-    },
     optionBarButton: {
         marginRight: 50,
         marginLeft: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'maroon',
+        borderRadius: 40,
+        marginTop: 10,
+        marginBottom: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        width: '60%',
+        alignSelf: 'center',
     },
     optionText: {
         fontSize: 16,
+        lineHeight: 21,
+        letterSpacing: 0.25,
         fontWeight: 'bold',
         color: 'white',
     },
-    h1: { 
+    circle: {
+     backgroundColor: '#f52d56',
+     width: 60,
+     height: 60,
+     position: 'absolute',
+     bottom: 40,
+     right: 40,
+     borderRadius: 50,
+     justifyContent: 'center',
+     alignItems: 'center',
+    },
+    dropdownContainer: {
+        width: '90%',
+        alignSelf: 'center',
+        marginTop: 15,
+    },
+    h1: {
         fontSize: 20,
         fontWeight: 'bold',
     },
-    h2: { 
+    h2: {
         fontSize: 15,
         fontWeight: 'bold',
     },
@@ -61,9 +77,17 @@ const Theme = StyleSheet.create({
     },
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    container1: {
+    //add here!!
+    },
+    modalCentered: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background
     },
     view: {
         padding: 10,
