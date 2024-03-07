e03c9e8922fae052b11aa119692c4738b26ba85d
Chutiyada Cheepchiewcharnchai <psycc6@exmail.nottingham.ac.uk>
2024-03-07 16:34:19 +0000

    Fix global userTypeID query in app.js for less querying

--- rowing_club_app/App.js
+++ rowing_club_app/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { db } from './src/config/firebase.js';
+// rowing_club_app/src/config/firebase.js
+// rowing_club_app/App.js
+import { collection, onSnapshot, query, where } from "firebase/firestore";
 import ScreensContainer from './src/screensContainer';
 
 export default function App() {
+  // TEST USER VIEWS BY CHANGING global.user AND RELOADING THE APP
   global.user = "FWBWX7EOw75rwE20cQD2" //Coach
-  //global.user = "0Iz45PGQ70hFnUpo6loC" //14-15
+  // global.user = "0Iz45PGQ70hFnUpo6loC" //14-15
   // global.user = "njxPjxAazoa9feL0eeKF" //18 and Older
+
+  //const
+  global.userTypeID
+
+  // query for user type
+  useEffect(() => {
+    // query for current user information
+    const q = query(collection(db, "User"), where("__name__", "==", global.user));
+    const querySnapshot = onSnapshot(q, (snapshot) => {
+      global.userTypeID = snapshot.docs[0].data().TypeID
+    });
+
+    // return cleanup function
+    return () => querySnapshot();
+}, [global.user]);
 
   return (
     <ScreensContainer/>
--- rowing_club_app/src/screens/events/events.js
+++ rowing_club_app/src/screens/events/events.js
@@ -2,30 +2,21 @@ import React from 'react';
 import EventsCoach from './eventsCoach';
 import EventsRower from './eventsRower';
 import { useState, useEffect } from 'react';
-import { db } from '../../config/firebase';
-import { collection, onSnapshot, query, where } from "firebase/firestore";
 
 export default function EventsScreen({ navigation }) {
     // const
     const [content, setContent] = useState();
 
-    // query user type
+    // check if user is coach or rower
     useEffect(() => {
-        // query for current user information
-        const q = query(collection(db, "User"), where("__name__", "==", global.user));
-        const querySnapshot = onSnapshot(q, (snapshot) => {
-            if (snapshot.docs[0].data().TypeID === "YDYsOFRCBMqhFpDn1buu") {
-                // set returning content to coach screen
-                setContent(<EventsCoach />);
-            } else {
-                // set returning content to rower screen
-                setContent(<EventsRower />);
-            }
-        });
-
-        // return cleanup function
-        return () => querySnapshot();
-    }, []);
+        if (global.userTypeID === "YDYsOFRCBMqhFpDn1buu") {
+            // set returning content to coach screen
+            setContent(<EventsCoach />);
+        } else {
+            // set returning content to rower screen
+            setContent(<EventsRower />);
+        }
+    }, [global.userTypeID]);
 
     return content;
 }
