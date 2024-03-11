96968914ba8a62fb7779e1027349eb05a79b7437
Yasmin Gordon <psyyg4@nottingham.ac.uk>
2024-03-11 18:05:55 +0000

    Home Page - chnaging to global variables

--- rowing_club_app/src/screens/home/home.js
+++ rowing_club_app/src/screens/home/home.js
@@ -7,16 +7,28 @@ export default function HomeScreen({ navigation }) {
     // const
     const [content, setContent] = useState();
 
+    //userType = "Coach"
+    //userType = "14-15"
+
+    //global.userTypeID = "YDYsOFRCBMqhFpDn1buu" //coach
+    global.userTypeID = "AmU8s77q7TcDytflxrC8" //rower
+    
     // check if user is coach or rower
     useEffect(() => {
-        if (global.userTypeID === "YDYsOFRCBMqhFpDn1buu") {
+        if (global.userTypeID == "YDYsOFRCBMqhFpDn1buu") {
             // set returning content to coach screen
             setContent(<HomeCoach />);
+            console.log("COACH");
         } else {
-            // set returning content to rower screen
+            //// set returning content to rower screen
             setContent(<HomeRower />);
+            console.log("ROWER");
+            console.log("Test 1: " + global.userTypeID);
+            console.log("YDYsOFRCBMqhFpDn1buu");
         }
+
     }, [global.userTypeID]);
 
     return content;
+
 }
