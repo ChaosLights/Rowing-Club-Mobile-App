5cf789424e097999e791cbd593ad2fc33fa476db
Yasmin Gordon <psyyg4@nottingham.ac.uk>
2024-03-13 19:11:41 +0000

    Home Page - correcting deleting userID when the user switch thier attendance options

--- rowing_club_app/App.js
+++ rowing_club_app/App.js
@@ -11,8 +11,8 @@ export default function App() {
   //global.user = "FWBWX7EOw75rwE20cQD2" //Coach
   //global.user = "0Iz45PGQ70hFnUpo6loC" //14-15 (Jason)
   //global.user = "YRhW9fMSA0hd6IixgLaO" //16-17 (Freddy)
-  global.user = "njxPjxAazoa9feL0eeKF" //18 and Older (Diana)
-  //global.user = "hGSQNMnQa4Bjt0zb0L5i" //18 and Older (Jack)
+  //global.user = "njxPjxAazoa9feL0eeKF" //18 and Older (Diana)
+  global.user = "hGSQNMnQa4Bjt0zb0L5i" //18 and Older (Jack)
 
   //const
   global.userTypeID
@@ -25,9 +25,9 @@ export default function App() {
       global.userTypeID = snapshot.docs[0].data().TypeID
       console.log("Test 2: " + userTypeID);
     });
-
+ 
     // return cleanup function
-    return () => querySnapshot();
+    return () => querySnapshot(); 
 }, [global.user]);
 
   return (
