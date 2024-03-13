de9849d8d61659ded2ac311980d4d52567114bf4
Yasmin Gordon <psyyg4@nottingham.ac.uk>
2024-03-13 16:40:31 +0000

    Home Page - adding attendance data to availabiliy collection

--- rowing_club_app/src/screens/home/home.js
+++ rowing_club_app/src/screens/home/home.js
@@ -11,7 +11,7 @@ export default function HomeScreen({ navigation }) {
     //userType = "14-15"
 
     //global.userTypeID = "YDYsOFRCBMqhFpDn1buu" //coach
-    global.userTypeID = "AmU8s77q7TcDytflxrC8" //rower
+    //global.userTypeID = "AmU8s77q7TcDytflxrC8" //rower
 
     // check if user is coach or rower
     useEffect(() => {
--- rowing_club_app/src/screens/home/homeRower.js
+++ rowing_club_app/src/screens/home/homeRower.js
@@ -3,7 +3,7 @@ import { View, Text, ScrollView, FlatList } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { SelectList } from 'react-native-dropdown-select-list'
 import { db } from '../../config/firebase';
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, doc, setDoc, arrayUnion, updateDoc, arrayRemove } from "firebase/firestore";
 import Theme from '../../style';
 
 export default function HomeScreen({ navigation }) {
@@ -20,18 +20,17 @@ export default function HomeScreen({ navigation }) {
     const typeID = global.userTypeID;
 
     const AttendancePickerData = [
-        {key:'Not Attending', value:'Absent'},
-        {key:'Attending', value:'Attending'},
-        {key:'Sick', value:'Sick'},
-        {key:'Home Training', value:'Home Training'},
-    
-      ]
-    
-      const WeekPickerData = [
-        {key:'Current Week', value:'Current week'},
-        {key:'Next Week', value:'Next week'},
-    
-      ]
+        { key: 'Not Attending', value: 'Absent' },
+        { key: 'Attending', value: 'Attending' },
+        { key: 'Sick', value: 'Sick' },
+        { key: 'Home Training', value: 'Home Training' },
+    ]
+
+    const WeekPickerData = [
+        { key: 'Current Week', value: 'Current week' },
+        { key: 'Next Week', value: 'Next week' },
+
+    ]
 
     //GET GROUP ATTENDANCE SCHEDULE
     //from RecuringSchedule db
@@ -69,19 +68,19 @@ export default function HomeScreen({ navigation }) {
                 {item.Overview}
             </Text>
             <Text style={Theme.body}>
-            {item.Description}
+                {item.Description}
             </Text>
         </View>
     );
 
     // Function to handle attendance selection
-    const handleAttendanceSelection = (dayTime, value) => {
+    const handleAttendanceSelection = async (dayTime, value) => {
         console.log(`Session ${dayTime} attendance set to: ${value}`);
         setSessionAttendance(prevState => ({
             ...prevState,
             [dayTime]: value
         }));
-        
+
         // Update availability list
         setAvailability(prevState => {
             const updatedAvailability = [...prevState];
@@ -91,9 +90,40 @@ export default function HomeScreen({ navigation }) {
             } else {
                 updatedAvailability.push({ dayTime, value });
             }
-            console.log(availability);
             return updatedAvailability;
         });
+
+        console.log(availability);
+        try {
+            // Attempt to create the document reference
+            const sessionDocRef = doc(db, "Availability", `${typeID}-${dayTime}`);
+            console.log("Session document reference:", sessionDocRef);
+
+            const sessionData = {
+                Session: dayTime,
+                TypeID: typeID
+            };
+
+            await setDoc(sessionDocRef, sessionData, { merge: true });
+            console.log("Session document created or updated successfully.");
+
+            const updateField = { [value]: arrayUnion(global.user) };
+            // Remove user's ID from the old field if it's different from the new field
+            for (const attendanceField of ['Attending', 'Absent', 'Sick', 'HomeTraining']) {
+                if (attendanceField !== value && sessionData.hasOwnProperty(attendanceField)) {
+                    // Check if the user's ID exists in the old field before attempting to remove it
+                    if (sessionData[attendanceField]) {
+                        // Remove user's ID from the old field and add it to the update object
+                        updateField[attendanceField] = arrayRemove(global.userID);
+                    }
+                }
+            }
+            await updateDoc(sessionDocRef, updateField);
+            console.log("Attendance updated successfully.");
+        } catch (error) {
+            // Log any errors encountered during document reference creation
+            console.error("Error creating session document reference:", error);
+        }
     }
 
     // displays week titles
@@ -108,12 +138,12 @@ export default function HomeScreen({ navigation }) {
                 placeholder='Current week'
                 save="value"
                 search={false}
-                boxStyles={{backgroundColor:'maroon'}}
-                dropdownStyles={{backgroundColor:'maroon'}}
-                dropdownTextStyles={{color:'white'}}
-                inputStyles={{color:'white'}}
+                boxStyles={{ backgroundColor: 'maroon' }}
+                dropdownStyles={{ backgroundColor: 'maroon' }}
+                dropdownTextStyles={{ color: 'white' }}
+                inputStyles={{ color: 'white' }}
             />
-            <Text style={Theme.h2}>{"\n"}Sessions:
+            <Text style={Theme.h2}>{"\n"}Sessions: {global.user}
             </Text>
             <View style={{ flexDirection: 'row' }}>
                 {selectedWeek === 'Current week' && (
@@ -151,51 +181,52 @@ export default function HomeScreen({ navigation }) {
     // (called in renderAttendance function)
     const renderWeek = (sessions, weekdays, offset) => {
         return weekdays.map((dayObj, index) => {
-        // Create a new Date object for the current day in the loop
-        const targetDate = new Date(dayObj.fullDate);
-        // Add the offset value to the day, affecting the date displayed
-        targetDate.setDate(targetDate.getDate() + offset);
-    
-        // Filter the sessions array to only include sessions for the current day
-        const sessionsForDay = sessions.filter((s) => {
-            const [day, time] = s.split(', ');
-            // Compare the day in the session data with the current day's localized name
-            return day === targetDate.toLocaleDateString(undefined, { weekday: 'long' });
-        });
-    
-        return (
-            // prints weekday and training times
-            <View key={index} style={Theme.eventContainer}>
-            <Text style={[Theme.h3, { marginBottom: 10 }]}>
-                {/* Display the full date (weekday, month, day, and year) for the current day */}
-                {targetDate.toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</Text>
-            {sessionsForDay.length > 0 ? (
-                sessionsForDay.map((session, sessionIndex) => {
-                const [day, time] = session.split(', ');
-                const dayTime = `${day}, ${time}`;
-                return (
-                    <View key={sessionIndex} style={[Theme.GreyOvalButton, { flexDirection: 'row', justifyContent: 'space-between' }]}>
-                    <Text style={{ color: 'white' }}>{time}</Text>
-                    {/* Pass the dayTime and session data to handleAttendanceSelection when an option is selected */}
-                    <SelectList
-                        setSelected={(val) => handleAttendanceSelection(dayTime, val)}
-                        data={AttendancePickerData}
-                        
-                        search={false}
-                        onChange={(val) => console.log('Selected:', val)} // Add an onChange handler to log the selected value
-                        placeholder={"Select"}
-                        save="value"
-                        boxStyles={{backgroundColor:'#F5F5F5'}}
-                        dropdownStyles={{backgroundColor:'#F5F5F5'}}
-                    />
-                    </View>
-                )
-                })
-            ) : (
-                <Text>No session{"\n"}</Text>
-            )}
-            </View>
-        );
+            // Create a new Date object for the current day in the loop
+            const targetDate = new Date(dayObj.fullDate);
+            // Add the offset value to the day, affecting the date displayed
+            targetDate.setDate(targetDate.getDate() + offset);
+
+            // Filter the sessions array to only include sessions for the current day
+            const sessionsForDay = sessions.filter((s) => {
+                const [day, time] = s.split(', ');
+                // Compare the day in the session data with the current day's localized name
+                return day === targetDate.toLocaleDateString(undefined, { weekday: 'long' });
+            });
+
+            return (
+                // prints weekday and training times
+                <View key={index} style={Theme.eventContainer}>
+                    <Text style={[Theme.h3, { marginBottom: 10 }]}>
+                        {/* Display the full date (weekday, month, day, and year) for the current day */}
+                        {targetDate.toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</Text>
+                    {sessionsForDay.length > 0 ? (
+                        sessionsForDay.map((session, sessionIndex) => {
+                            const date = targetDate.toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+                            const [day, time] = session.split(', ');
+                            const dayTime = `${date}, ${time}`;
+                            return (
+                                <View key={sessionIndex} style={[Theme.GreyOvalButton, { flexDirection: 'row', justifyContent: 'space-between' }]}>
+                                    <Text style={{ color: 'white' }}>{time}</Text>
+                                    {/* Pass the dayTime and session data to handleAttendanceSelection when an option is selected */}
+                                    <SelectList
+                                        setSelected={(val) => handleAttendanceSelection(dayTime, val)}
+                                        data={AttendancePickerData}
+
+                                        search={false}
+                                        onChange={(val) => console.log('Selected:', val)} // Add an onChange handler to log the selected value
+                                        placeholder={"Select"}
+                                        save="value"
+                                        boxStyles={{ backgroundColor: '#F5F5F5' }}
+                                        dropdownStyles={{ backgroundColor: '#F5F5F5' }}
+                                    />
+                                </View>
+                            )
+                        })
+                    ) : (
+                        <Text>No session{"\n"}</Text>
+                    )}
+                </View>
+            );
         });
     };
 
@@ -203,22 +234,22 @@ export default function HomeScreen({ navigation }) {
     // prints headings and calls methods renderNotification and renderAttendance to display info
     return (
         <FlatList
-          data={[
-            { sectionTitle: "Notifications", data: notification },
-            { sectionTitle: "Attendance", data: attendance }
-          ]}
-          renderItem={({ item }) => (
-            <View style={Theme.view}>
-              <Text style={Theme.h1}>{item.sectionTitle}</Text>
-              <FlatList
-                data={item.data}
-                renderItem={item.sectionTitle === "Notifications" ? renderNotification : renderAttendance}
-                keyExtractor={item => item.id}
-              />
-            </View>
-          )}
-          keyExtractor={(item, index) => index.toString()}
+            data={[
+                { sectionTitle: "Notifications", data: notification },
+                { sectionTitle: "Attendance", data: attendance }
+            ]}
+            renderItem={({ item }) => (
+                <View style={Theme.view}>
+                    <Text style={Theme.h1}>{item.sectionTitle}</Text>
+                    <FlatList
+                        data={item.data}
+                        renderItem={item.sectionTitle === "Notifications" ? renderNotification : renderAttendance}
+                        keyExtractor={item => item.id}
+                    />
+                </View>
+            )}
+            keyExtractor={(item, index) => index.toString()}
         />
-      );
-      
+    );
+
 }
